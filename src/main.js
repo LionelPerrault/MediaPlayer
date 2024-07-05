24cbac1934aece89e7bf31317b53598cee2d7aff
LionelPerrault <lionelperrault0616@gmail.com>
2024-07-05 05:02:13 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -22,11 +22,11 @@ window.resetApp = () => {
   });
   return '已重置应用，请刷新页面（按Ctrl/Command + R）';
 };
-console.log(
-  '如出现问题，可尝试在本页输入 %cresetApp()%c 然后按回车重置应用。',
-  'background: #eaeffd;color:#335eea;padding: 4px 6px;border-radius:3px;',
-  'background:unset;color:unset;'
-);
+// console.log(
+//   '如出现问题，可尝试在本页输入 %cresetApp()%c 然后按回车重置应用。',
+//   'background: #eaeffd;color:#335eea;padding: 4px 6px;border-radius:3px;',
+//   'background:unset;color:unset;'
+// );
 
 Vue.use(
   VueGtag,
